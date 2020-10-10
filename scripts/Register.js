617c0d793c90b5d32a7060a4d70bd21461177cba
Carla Navia <carlanavia@MBP-de-Carla.home>
2020-10-10 14:00:56 +0200

    Validator

--- scripts/Register.js
+++ scripts/Register.js
@@ -14,25 +14,25 @@ class Register {
         this.messageWrapper = document.querySelector(".message-container");
     }
 
-// Métodos para validar o manejar la información que insertamos en los inputs
-//no me funciona el target value... PREGUNTAR (min 21 del vídeo 1)
+    // Métodos para validar o manejar la información que insertamos en los inputs
+    //no me funciona el target value... PREGUNTAR (min 21 del vídeo 1)
 
-handleEmailInput = (event) => {
-    const email = event.target.value;
-    console.log(email)
-}
+    handleEmailInput = (event) => {
+        const email = event.target.value;
+        console.log(email)
+    }
 
-handlePasswordInput = (event) => {
-    const password = event.target.value;
-}
+    handlePasswordInput = (event) => {
+        const password = event.target.value;
+    }
 
-handleRepeatPasswordInput = (event) => {
-    const repeatPassword = event.target.value;
-}
+    handleRepeatPasswordInput = (event) => {
+        const repeatPassword = event.target.value;
+    }
 
-//Almacenar datos que hemos recogido en los métodos anteriores. Se ejecuta cuando el ususario envía el formulario 
-saveData = (event) => {
-//Previene el comportamiento por default del evento y no permite que la página cargue de nuevo
+    //Almacenar datos que hemos recogido en los métodos anteriores. Se ejecuta cuando el ususario envía el formulario 
+    saveData = (event) => {
+    //Previene el comportamiento por default del evento y no permite que la página cargue de nuevo
     event.preventDefault();
 
     const name = this.nameInput.value;
@@ -55,34 +55,33 @@ saveData = (event) => {
 
     const newUser = new User(name, lastName, email, password);
 
-// Guardaremos el usuario en la base de datos. Llámamos a database y creamos el nuevo usuario
-//database.createNewUser = (newUser); [esto era antes de crear el Database.js, lo substituimos por lo siguiente]
+    // Guardaremos el usuario en la base de datos. Llámamos a database y creamos el nuevo usuario
+    //database.createNewUser = (newUser); [esto era antes de crear el Database.js, lo substituimos por lo siguiente]
 
-db.saveNewUser(newUser);
-
-
-//Vaciar formulario
-this.nameInput.value = '';
-this.lastNameInput.value = '';
-this.emailInput.value = '';
-this.passwordInput.value ='';
-this.repeatPasswordInput.value = '';
-
-}
+    db.saveNewUser(newUser);
 
 
-// Registrar métodos para cada uno de los inputs
+    //Vaciar formulario
+    this.nameInput.value = '';
+    this.lastNameInput.value = '';
+    this.emailInput.value = '';
+    this.passwordInput.value ='';
+    this.repeatPasswordInput.value = '';
 
-addListeners = () => {
-// En los inputs crear un evento. Para vincular lo que pasa en los inputs con las funciones que van a hacer algo con esos inputs
-    this.emailInput.addEventListener('input', this.handleEmailInput);
+    }
 
-    this.passwordInput.addEventListener('input', this.handleRepeatPasswordInput);
 
-    this.repeatPasswordInput.addEventListener ('input', this.handleRepeatPasswordInput);
+    // Registrar métodos para cada uno de los inputs
+    addListeners = () => {
+    // En los inputs crear un evento. Para vincular lo que pasa en los inputs con las funciones que van a hacer algo con esos inputs
+        this.emailInput.addEventListener('input', this.handleEmailInput);
 
-    this.buttonInput.addEventListener ('click', this.saveData);
-}
+        this.passwordInput.addEventListener('input', this.handleRepeatPasswordInput);
+
+        this.repeatPasswordInput.addEventListener ('input', this.handleRepeatPasswordInput);
+
+        this.buttonInput.addEventListener ('click', this.saveData);
+    }
 
 }
 
---
+++ scripts/Validator.js
@@ -0,0 +1,85 @@
+"use strict";
+//Verificar email, constraseña...
+class Validator{
+constructor(){
+    this.invalidEmailError = "Please, enter a valid email";
+    this.emailExistsError = "This email is already registered";
+    this.passwordError = "The password must have minimum 7 characters";
+    this.repeatPasswordError = "Password do not match";
+    
+    //this.errors es un objeto que guardará los mensajes de arriba. No ponemos el error de email existente porque no hemos llamado a la Base de Datos para comprobarlo
+    this.errors = {
+        invalidEmailError: this.invalidEmailError,
+        passwordError: this.passwordError,
+        repeatPasswordError: this.repeatPasswordError
+    }
+}
+//Todo lo que queremos validar
+    validateValidEmail = (email) => {
+        if(this.emailIsValid(email)) {
+            delete this.errors.invalidEmailError
+        } else {
+            this.errors.invalidEmailError = this.invalidEmailError;
+        }
+    }
+
+//Verificar si el email que le ha introducido el usuario cumple con la especificación que ponemos en emailRegEx
+    emailIsValid = (email) => {
+        const emailRegEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+
+        const isValid = emailRegEx.test(email);
+        return isValid
+    }
+
+//Comprobar si el email que ingresamos en el input es válido o no. Creamos una variable que sea igual al método que hemos creado en Database.js
+    validateUniqueEmail = (newEmail) => {
+        const userDb = db.getAllUsers
+        //Comprobar que el array de getAllUsers haya vuelto con algo
+        if (userDb) {
+            let emailUnique = true;
+            //iterar todas las posiciones del array para que lo busque. En caso de que encuentre uno igual, cambiamos el valo de unique a false
+            userDb.forEach(user => {
+                if(user.email == newEmail){
+                    emailUnique = false;
+                }
+            });
+            //si el email es único, decimos que no aparezca el error. Si el email está repetido, decimos que aparezca el error
+            if (emailUnique){
+                delete this.errors.emailExistsError;
+            } else {
+                this.errors.emailExistsError = this.emailExistsError; //Creamos el objeto de emailEcistsError dentro de errors
+            }
+        }
+    }
+
+//La contraseña debe tener mínimo 7 carácteres
+    validatePassword = (password) => {
+        if (password.length > 6) {
+            delete this.errors.passwordError;
+        } else {
+            this.errors.passwordError = this.passwordError;
+        }
+    }
+
+    validatePasswordRepeat = (password, passwordrepeat) => {
+        if (password == passwordrepeat) {
+            delete this.errors.repeatPasswordError;
+        } else {
+            this.errors.repeatPasswordError = this.repeatPasswordError;
+        }
+    }
+
+    getErrors = () => {
+        return this.errors;
+    }
+//Cada vez que cargas la página, creamos un nuevo validator
+    resetValidator = () => {    
+        this.errors = {
+            invalidEmailError: this.invalidEmailError,
+            passwordError: this.passwordError,
+            repeatPasswordError: this.repeatPasswordError
+        }
+    }
+}
+//Crear nueva instancia para llamar a las funciones con Validator
+const validator = new Validator();
