ffd5687b361bcd4c5efbbf8565d8a8ef88616708
Carla Navia <carlanavia@MacBook-Pro-de-Carla.local>
2020-10-11 13:56:09 +0200

    MovieDbApiService

--- scripts/MovieDbApiService.js
+++ scripts/MovieDbApiService.js
@@ -1,8 +1,24 @@
-const API_KEY = "01f37d24238fcae1de083c92988ee412";
-const BASE_URL = "https://api.themoviedb.org/3/";
+class MovieDbApiService {
+  constructor() {
+    this.BASE_URL = "https://api.themoviedb.org/3/";
+    this.API_KEY = "01f37d24238fcae1de083c92988ee412";
+    this.getMoviesUrl = `${this.BASE_URL}search/movie?api_key=${this.API_KEY}&query=`;
+    this.getMovieUrl = `${this.BASE_URL}movie/`
+  }
 
-const getMoviesUrl = `${BASE_URL}search/movie?api_key=${API_KEY}&query=spiderman`
-
-fetch(getMoviesUrl)
-  .then(response => response.json())
-  .then(data => console.log(data));
+  //La crida de la API per tenir el llistat de pelis que s'estàn buscant per text
+  getMovies = (searchText) => {
+    fetch(`${this.getMoviesUrl}${searchText}`)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
+  //La crida de la API per tenir el detall de la peli
+  getMovie = (id) => {
+    fetch(`${this.getMovieUrl}${id}?api_key=${this.API_KEY}`)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
+}
+const moviesService = new MovieDbApiService();
+moviesService.getMovies("spiderman");
+moviesService.getMovie("557");
